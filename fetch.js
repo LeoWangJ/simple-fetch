a4784a8632e93c5a83a5dfd63196b79f0951cde7
leowang <rfv7855659@gmail.com>
2019-07-05 03:06:06 +0800

    新增request 方法來處理使用者傳遞的參數

--- fetch.js
+++ fetch.js
@@ -1,20 +1,42 @@
 (function(self){
-    
-    self.fetch = function(url) {
+
+    let methods = ['DELETE', 'GET', 'HEAD', 'OPTIONS', 'POST', 'PUT']
+    function normalizeMethod (method) {
+        let upper = method.toUpperCase()
+        return methods.indexOf(upper) > -1 ? upper : method
+    }
+    function Request (input, options) {
+        options = options || {}
+        
+        //input 參數有 Resquest實例或者字串
+        if(input instanceof Request) {
+
+        } else {
+            this.url = String(input)
+        }
+
+        this.method = normalizeMethod(options.method || this.method || 'GET')
+    }
+
+    self.fetch = function(url, init) {
         return new Promise((resolve, reject) =>{
-            let req = new XMLHttpRequest()
-            req.open('GET', url, true)
-            req.onload = () => {
-                if (req.status === 200) {
-                    resolve(req.responseText)
+
+            let request = new Request(url, init)
+
+            let xhr = new XMLHttpRequest()
+            xhr.open(request.method, request.url, true)
+
+            xhr.onload = () => {
+                if (xhr.status === 200) {
+                    resolve(xhr.responseText)
                 } else {
-                    reject(new Error(req.statusText))
+                    reject(new Error(xhr.statusText))
                 }
             }
-            req.onerror = () => {
-                reject(new Error(req.statusText))
+            xhr.onerror = () => {
+                reject(new Error(xhr.statusText))
             }
-            req.send()
+            xhr.send()
         })
     }
 })(typeof self !== undefined ? self : this)
